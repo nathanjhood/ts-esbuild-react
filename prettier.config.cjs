59e3ca83e6b1a835bf4ac66842e228a99cc590db
StoneyDSP <nathanjhood@googlemail.com>
2024-08-10 19:06:52 +0200

    migrated to modern prettier config
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'none',
+  jsxBracketSameLine: true
+};
+
+module.exports = config;
