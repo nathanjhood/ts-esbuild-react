88115c0f284ec1e5c6d33981479a6c77b7fff4dc
StoneyDSP <nathanjhood@googlemail.com>
2024-08-10 19:06:32 +0200

    migrated to modern eslint config
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-const restrictedGlobals = require('confusing-browser-globals');
-
-/** @type { import("eslint").ESLint.ConfigData } */
-module.exports = {
-  extends: ["react-app", "react-app/jest"],
-  rules: {
-    'no-restricted-globals': ['error'].concat(restrictedGlobals),
-  }
-};
---
+++ eslint.config.cjs
@@ -0,0 +1,55 @@
+const restrictedGlobals = require('confusing-browser-globals');
+const eslint = require('@eslint/js');
+const react = require('eslint-plugin-react');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const eslintPluginReact = require('eslint-plugin-react');
+const globals = require('globals');
+
+/** @type { import("eslint").Linter.Config[] } */
+const config = [
+  eslint.configs.recommended,
+  eslintPluginReact.configs.flat.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    // extends: ["react-app", "react-app/jest"],
+    files: ['{src,lib}/**/*.{js,cjs.mjs,ts,cts,mts}', 'src/**/*.{jsx,tsx}'],
+    ignores: ['**/*.config.{js,cjs.mjs,ts,cts,mts}'],
+    rules: {
+      'no-restricted-globals': ['error'].concat(restrictedGlobals),
+      semi: 'warn',
+      'prefer-const': 'warn',
+      'no-unused-vars': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error'
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error'
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+          // ecmaVersion: "latest",
+          // sourceType: "module"
+        },
+        globals: {
+          ...globals.browser
+        }
+      }
+    },
+    // env: {
+    //   "browser": true,
+    //   "es2021": true,
+    //   "jest": true
+    // },
+    // extends: [
+    //   // "react-app", "react-app/jest"
+    // ],
+    plugins: { react }
+  }
+];
+
+module.exports = config;
