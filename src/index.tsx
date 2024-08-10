79cb1df868569f56e762e73bb12de7a455561917
StoneyDSP <nathanjhood@googlemail.com>
2024-08-10 22:04:31 +0200

    added index declarations
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
+// import 'client-only';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import './index.css';
+
+import App from './App';
+
+// eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
