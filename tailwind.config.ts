8ce80b5e9eb39fbf8f00772a06a76415dd495d5c
StoneyDSP <nathanjhood@googlemail.com>
2024-08-10 17:44:50 +0200

    added tailwindcss
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export const config: Config = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
