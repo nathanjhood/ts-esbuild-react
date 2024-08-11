0862101175daf6d7ec492b0b0d101ca22e5b8cab
StoneyDSP <nathanjhood@googlemail.com>
2024-08-11 19:53:26 +0200

    implemented eslint config for prettier
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ .eslint/prettier.mjs
@@ -0,0 +1,82 @@
+// @ts-check
+import * as tseslint from 'typescript-eslint';
+import * as base from './base.mjs';
+import prettier from 'eslint-config-prettier';
+
+/**
+ * An string to identify the configuration object. Used in error messages and inspection tools.
+ * @name name
+ * @type {string | typeof base['name']}
+ */
+export const name = 'prettier';
+
+/**
+ * An array of glob patterns indicating the files that the configuration object should apply to. If not specified, the configuration object applies to all files matched by any other configuration object.
+ * @name files
+ * @type {undefined | typeof base['files']}
+ */
+export const files = undefined;
+
+/**
+ * An array of glob patterns indicating the files that the configuration object should not apply to. If not specified, the configuration object applies to all files matched by files.
+ * @name ignores
+ * @type {undefined | typeof base['ignores']}
+ */
+export const ignores = undefined;
+
+/**
+ * An object containing a name-value mapping of plugin names to plugin objects. When files is specified, these plugins are only available to the matching files.
+ * @name plugins
+ * @type {undefined | typeof base['plugins']}
+ */
+export const plugins = undefined;
+
+/**
+ * An object containing name-value pairs of information that should be available to all rules.
+ * @name settings
+ * @type {undefined | typeof base['settings']}
+ */
+export const settings = undefined;
+
+/**
+ * An object containing the configured rules. When files or ignores are specified, these rule configurations are only available to the matching files.
+ * @name rules
+ * @type {undefined | typeof base['rules']}
+ */
+export const rules = prettier.rules;
+
+/**
+ * An object containing settings related to how JavaScript is configured for linting.
+ * @name languageOptions
+ * @type {undefined | typeof base['languageOptions']}
+ */
+export const languageOptions = undefined;
+
+/**
+ * An object containing settings related to the linting process.
+ * @name linterOptions
+ * @type {undefined | typeof base['linterOptions']}
+ */
+export const linterOptions = undefined;
+
+/**
+ * @type {undefined | typeof base['processor']}
+ */
+export const processor = undefined;
+
+/**
+ * @name default
+ */
+export default (() => {
+  return {
+    name: name,
+    // files: files,
+    // ignores: ignores,
+    // plugins: plugins,
+    // settings: settings,
+    rules: rules,
+    // languageOptions: languageOptions,
+    // linterOptions: linterOptions,
+    // processor: processor,
+  }
+})()
