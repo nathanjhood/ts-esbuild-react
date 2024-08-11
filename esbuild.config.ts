fc5b17df166bdd25ae2d98dccd9165c462c63aaa
StoneyDSP <nathanjhood@googlemail.com>
2024-08-11 22:52:21 +0200

    esbuild config cleanup
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -1,59 +1,100 @@
-/// <reference types="esbuild" />
+// @ts-check
+
+import * as esbuild from 'esbuild';
+import esbuildPluginTsc from 'esbuild-plugin-tsc';
+import esbuildPluginCopy from 'esbuild-plugin-copy';
+import esbuildPluginClean from 'esbuild-plugin-clean';
+
 
 import { fileURLToPath, URL } from 'url'
-import { spawn } from 'child_process';
 
-import * as esbuild from 'esbuild'
+// declare type BuildPaths = {
+//   publicSrcDir?: string | URL;
+//   publicOutputDir?: string | URL;
+//   publicOutFile?: string | URL;
+//   srcHtmlFile?: string | URL;
+//   destinationHTML?: string | URL;
+// }
 
-declare type BuildPaths = {
-  publicSrcDir?: string | URL;
-  publicOutputDir?: string | URL;
-  publicOutFile?: string | URL;
-  srcHtmlFile?: string | URL;
-  destinationHTML?: string | URL;
-}
+// declare type TscOptions = {
+//   tsconfigPath?: string;
+//   force?: boolean;
+//   tsx?: boolean;
+// }
 
-declare type TscOptions = {
-  tsconfigPath?: string;
-  force?: boolean;
-  tsx?: boolean;
-}
+// declare type CopyOptions = (import('esbuild-plugin-copy').Options)
+// declare type CleanOptions = (import('esbuild-plugin-clean').CleanOptions)
+// declare type BuildOptions = (import('esbuild').BuildOptions)
 
-declare type CopyOptions = (import('esbuild-plugin-copy').Options)
-declare type CleanOptions = (import('esbuild-plugin-clean').CleanOptions)
-declare type BuildOptions = (import('esbuild').BuildOptions)
 
-declare const esbuildPluginTsc: typeof import('esbuild-plugin-tsc')
-declare const esbuildPluginCopy: typeof import('esbuild-plugin-copy').copy
-declare const esbuildPluginClean: typeof import('esbuild-plugin-clean').clean
 
-const config = (() => {
+/**
+ * @param  {esbuild.BuildOptions} options
+ * @return {esbuild.BuildOptions}
+ */
+export const createBuildOptions = (options: esbuild.BuildOptions): esbuild.BuildOptions => {
+  // const srcSourceDir = fileURLToPath(new URL('src', import.meta.url));
+  // const srcOutputDir = fileURLToPath(new URL('dist', import.meta.url));
+  const publicSourceDir = fileURLToPath(new URL('public', import.meta.url));
+  const publicOutputDir = fileURLToPath(new URL('dist', import.meta.url));
 
-  const publicSrcDir = './public'
-  const publicOutputDir = './dist'
-  const publicOutFile = `${publicOutputDir}/app.js`
-  const srcHtmlFile = `./${publicSrcDir}/index.html`
-  const destinationHTML = `${publicOutputDir}/index.html`
+  // const publicOutFile = `${publicOutputDir}/app.js`;
+  // const srcHtmlFile = `${publicSrcDir}/index.html`;
+
+  // const appSourceFile = fileURLToPath(new URL('App.tsx', srcSourceDir))
+  // const indexSourceFile = fileURLToPath(new URL('index.tsx', srcSourceDir))
+  const destinationHTML = `${publicOutputDir}/index.html`;
 
   return {
-    external: ['react', 'react-dom'],
-    entryPoints: ['src/index.tsx'],
-    outfile: fileURLToPath(new URL(`${publicOutputDir}/${publicOutFile}`, import.meta.url)),
+    // external: ['react', 'react-dom'],
+    entryPoints: [
+      fileURLToPath(new URL('src/App.tsx', import.meta.url)),
+      fileURLToPath(new URL('src/index.tsx', import.meta.url)),
+    ],
+    // outfile: fileURLToPath(new URL(publicOutFile, import.meta.url)),
+    outbase: fileURLToPath(new URL('src', import.meta.url)),
+    outdir: fileURLToPath(new URL('dist', import.meta.url)),
+
     sourcemap: true,
     bundle: true,
-    minify: false,
-		loader: {
-		  '.tsx': 'tsx',
-		  '.ts': 'ts',
-		  '.svg': 'dataurl'
-		},
+    minify: true,
+    // jsxDev: true,
+    // jsx: 'automatic',
+    loader: {
+      // '.tsx': 'tsx',
+      // '.ts': 'ts',
+      '.svg': 'dataurl'
+    },
     banner: {
       // NODE - Append Hot reload event listener to DOM
       // js: `new EventSource('/esbuild').addEventListener('change', () => location.reload());`,
-      // // BROSWER - Append Hot reload event listener to DOM
+      // // BROWSER - Append Hot reload event listener to DOM
       js: ' (() => new EventSource("/esbuild").onmessage = () => location.reload())(); '
-
     },
+    plugins: [
+      esbuildPluginTsc({
+        force: true,
+        tsconfigPath: fileURLToPath(new URL('tsconfig.json', import.meta.url)),
+        tsx: true
+      }),
+      esbuildPluginClean({
+        patterns: [`${publicOutputDir}/*`, `!${destinationHTML}`],
+        sync: true,
+        verbose: false
+      }),
+      esbuildPluginCopy({
+        copyOnStart: true,
+        resolveFrom: 'cwd',
+        assets: {
+          from: [`${publicSourceDir}/**/*`],
+          to: [`${publicOutputDir}`]
+          // keepStructure: true
+        },
+        verbose: false,
+        once: false,
+        globbyOptions: {}
+      })
+    ],
     // watch: {
     //   onRebuild(error) {
     //     clients.forEach(res => res.write('data: update\n\n'))
@@ -68,85 +109,8 @@ const config = (() => {
     //       )
     //   }
     // },
-    // define: {
-    //   'process.env.NODE_ENV': '"development"',
-    //   'process.env.DEBUG': '"FALSE"',
-    //   'process.env.GATEWAY_HOST': '"http://localhost"',
-    //   'process.env.GATEWAY_PORT': '"5000"'
-    // },
-    plugins: [
-      esbuildPluginTsc(Object.freeze<TscOptions>(
-        {
-          force: true,
-          tsconfigPath: fileURLToPath(new URL('./tsconfig.bundler.json', import.meta.url)),
-          tsx: true
-        } satisfies TscOptions)
-      ),
-      esbuildPluginClean(Object.freeze<CleanOptions>(
-        {
-          patterns: [
-            `${publicOutputDir}/*`,
-            `!${destinationHTML}`
-          ],
-          sync: true,
-          verbose: true,
-        } satisfies CleanOptions)
-      ),
-      esbuildPluginCopy(Object.freeze<CopyOptions>(
-        {
-          copyOnStart: true,
-          resolveFrom: 'cwd',
-          assets: {
-            from: [
-              `${publicSrcDir}/**/*`
-            ],
-            to: [
-              `${publicOutputDir}`
-            ],
-            // keepStructure: true
-          },
-          verbose: true,
-          once: false,
-          globbyOptions: {}
-        } satisfies CopyOptions)
-      )
-    ],
+    ...options // args
+  };
+};
 
-  } satisfies esbuild.BuildOptions
-})()
-
-/**
- * @param  {import("esbuild").BuildOptions | undefined} options
- * @return {import("esbuild").BuildOptions}
- */
-export const createEsbuildConfig = (options: esbuild.BuildOptions = config): esbuild.BuildOptions => {
-
-  // const publicSrcDir = './public'
-  // const publicOutputDir = './dist'
-  // const publicOutFile = `${publicOutputDir}/app.js`
-  // const srcHtmlFile = `./${publicSrcDir}/index.html`
-  // const destinationHTML = `${publicOutputDir}/index.html`
-
-  const defaults = { ...config } satisfies esbuild.BuildOptions
-  const args = { ...options } satisfies esbuild.BuildOptions
-
-  return {
-      ...defaults,
-      ...args
-    }
-
-}
-
-const createTscServer = () => {
-  const tsc = spawn('npx tsc', ['--noEmit --watch --skipLibCheck --pretty --project tsconfig.json'], {
-    shell: true
-  })
-  tsc.stdout.on('data', data => {
-    console.info(`${data}`)
-  })
-  tsc.on('error', error => {
-    console.error(`error: ${error.message}`)
-  })
-}
-
-export default createEsbuildConfig
+export default createBuildOptions;
