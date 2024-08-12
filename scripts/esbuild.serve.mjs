9f608b95f8f9f28da630859d4af19ff53e32b5e1
StoneyDSP <nathanjhood@googlemail.com>
2024-08-12 18:13:45 +0200

    fixed esbuild output type for browser builds

--- scripts/esbuild.serve.mjs
+++ scripts/esbuild.serve.mjs
@@ -1,14 +1,17 @@
 import * as esbuild from 'esbuild';
 import * as config from '../esbuild.config.mjs';
 import * as tsc from './tsc.serve.mjs';
+import process from 'process';
 
 const options = config.createBuildOptions({
   sourcemap: true,
   bundle: true,
-  // format: 'cjs',
+  minify: false,
+  treeShaking: true,
+  format: 'esm',
   banner: {
     // NODE - Append Hot reload event listener to DOM
-    // js: `new EventSource('/esbuild').addEventListener('change', () => location.reload());`,
+    // js: `new EventSource('/esbuild').addEventListener('change', () => location.reload());`
     // // BROSWER - Append Hot reload event listener to DOM
     js: ' (() => new EventSource("/esbuild").onmessage = () => location.reload())();'
   }
@@ -16,31 +19,31 @@ const options = config.createBuildOptions({
 
 const ctx = await esbuild.context(options);
 
-await tsc.createTscServer()
+await tsc.createTscServer();
 await ctx.watch();
 
-console.info("watching...")
+console.info('watching...');
 
 const { host, port } = await ctx.serve({
   port: 5500,
   host: '127.0.0.1',
   servedir: './dist',
-  fallback: "./dist/index.html"
+  fallback: './dist/index.html'
 });
 
-console.log(`Serving app at http://${host}:${port}.`);
+console.info(`Serving app at http://${host}:${port}.`);
 
 // Whenever we get some data over stdin
 process.stdin.on('data', async () => {
   try {
     // Cancel the already-running build
-    await ctx.cancel()
+    await ctx.cancel();
     // Then start a new build
-    console.info('build:', await ctx.rebuild())
+    console.info('build:', await ctx.rebuild());
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-})
+});
 
 // // exit routine
 // // await new Promise(r => setTimeout(r, 10 * 1000))
