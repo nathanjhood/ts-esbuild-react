2beddadf22a9f2da9a9fab795299ef8ede1ad2c9
StoneyDSP <nathanjhood@googlemail.com>
2024-08-15 16:58:01 +0200

    started Node env typings
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,22 @@
-/// <reference types="node" />
+declare global {
+    namespace NodeJS {
+      interface ProcessEnv {
+        [key: string]: string | undefined;
 
-/**
- * Add this to '.gitignore'
- */
+        NODE_ENV: 'development' | 'production';
+        
+        PWD: string;
+        SHELL: string;
+        PATH: string;
+
+        VERBOSE?: boolean | string;
+        
+        PORT?: string;
+        HOST?: string;
+      }
+    }
+  }
+  
+  // If this file has no import/export statements (i.e. is a script)
+  // convert it into a module by adding an empty export statement.
+  export {}
