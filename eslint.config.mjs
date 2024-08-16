2c1ecf92e0a8db70bb762bcd9fe0341abfbb384e
StoneyDSP <nathanjhood@googlemail.com>
2024-08-16 03:58:18 +0200

    added typings to eslint config
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
+//@ts-check
 
-export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintConfigPrettier, // Make sure to put it last, so it gets the chance to override other configs.
-];
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+/** @type {import('typescript-eslint').Config} */
+const configs = [
+    { files: ['**/*.{js,mjs,cjs,ts}'] },
+    { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    { rules: {} },
+    eslintConfigPrettier, // Make sure to put it last, so it gets the chance to override other configs.
+]
+
+export default configs
