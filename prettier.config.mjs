aa7fea8da6f4bbb0fadea8879ae7a346d484fe42
StoneyDSP <nathanjhood@googlemail.com>
2024-08-16 03:23:20 +0200

    added prettier config
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: 'es5',
+    tabWidth: 2,
+    semi: true,
+    singleQuote: false,
+    useTabs: false,
+    printWidth: 80,
+    endOfLine: 'auto',
+}
+
+export default config
