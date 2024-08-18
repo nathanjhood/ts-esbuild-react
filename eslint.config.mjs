3fffc048a9b887135a3ffd8e5179e4498a4b393c
StoneyDSP <nathanjhood@googlemail.com>
2024-08-18 18:45:00 +0200

    refreshed linter/formatter config - will wrap into a package
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,9 +14,10 @@ const configs = [
   ...tseslint.configs.recommended,
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
     },
   },
   eslintConfigPrettier, // Make sure to put it last, so it gets the chance to override other configs.
---
+++ packages/ts-esbuild/prettier.config.mjs
@@ -0,0 +1,17 @@
+//@ts-check
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: 'es5',
+    tabWidth: 2,
+    semi: true,
+    singleQuote: false,
+    useTabs: false,
+    printWidth: 80,
+    endOfLine: 'lf',
+}
+
+export default config
