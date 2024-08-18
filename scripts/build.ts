8933f0f1bf1a75671d7af8fdbf7aae38ddfb3d42
StoneyDSP <nathanjhood@googlemail.com>
2024-08-18 21:12:08 +0200

    added error cause if NODE_ENV throws an error
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,7 +3,7 @@
 // Do this as the first thing so that any code reading it knows the right env.
 // process.env.BABEL_ENV = 'production';
 if (process.env.NODE_ENV !== "production")
-  throw new Error("NODE_ENV is not production...");
+  throw new Error("NODE_ENV is not production...", { cause: process.env });
 
 import * as esbuild from "esbuild";
 
