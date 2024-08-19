9b189a5e049efdca942729974bd5c578986024c2
StoneyDSP <nathanjhood@googlemail.com>
2024-08-19 19:35:27 +0200

    added Jest config tp test parsed HTML with
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,8 +1,6 @@
-#!/usr/bin/env -S yarn tsx
-
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = "production";
-process.env.NODE_ENV = "production";
+process.env.BABEL_ENV = "test";
+process.env.NODE_ENV = "test";
 process.env.PUBLIC_URL = "";
 
 // Makes the script crash on unhandled rejections instead of silently
@@ -14,3 +12,40 @@ process.on("unhandledRejection", (err) => {
 
 // Ensure environment variables are read.
 require("../config/env");
+
+const jest: typeof import("jest") = require("jest");
+const execSync: typeof import("child_process").execSync =
+  require("child_process").execSync;
+// eslint-disable-next-line prefer-const
+let argv = process.argv.slice(2);
+
+function isInGitRepository() {
+  try {
+    execSync("git rev-parse --is-inside-work-tree", { stdio: "ignore" });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+function isInMercurialRepository() {
+  try {
+    execSync("hg --cwd . root", { stdio: "ignore" });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+// Watch unless on CI or explicitly running all tests
+if (
+  !process.env.CI &&
+  argv.indexOf("--watchAll") === -1 &&
+  argv.indexOf("--watchAll=false") === -1
+) {
+  // https://github.com/facebook/create-react-app/issues/5210
+  const hasSourceControl = isInGitRepository() || isInMercurialRepository();
+  argv.push(hasSourceControl ? "--watch" : "--watchAll");
+}
+
+jest.run(argv);
