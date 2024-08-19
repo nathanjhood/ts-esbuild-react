ca4e20f768e1e67483b8d75a1f4fa9c3fbcf17d6
StoneyDSP <nathanjhood@googlemail.com>
2024-08-19 17:33:19 +0200

    fixed a throw in scripts/start.ts
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- scripts/start.ts
+++ scripts/start.ts
@@ -2,6 +2,8 @@
 
 /// <reference lib="ESNext" />
 
+process.env.NODE_ENV = "development";
+
 // Do this as the first thing so that any code reading it knows the right env.
 // process.env.BABEL_ENV = "development";
 if (process.env.NODE_ENV !== "development")
